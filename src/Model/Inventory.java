b26ea5d8717ad75d211d31586ee2ef92a4d82d41
Jesse Merryman <71290104+jessemerryman@users.noreply.github.com>
2021-05-13 19:38:22 -0700

    Update Inventory.java

--- src/Model/Inventory.java
+++ src/Model/Inventory.java
@@ -36,29 +36,4 @@ public class Inventory {
     public static ObservableList<Product> getAllProducts() {
         return allProducts;
     }
-
-
-
-    /**
-     * - allParts:ObservableList<Part>
-     * - allProducts:ObservableList<Product>
-     * + addPart(newPart:Part):void
-     * + addProduct(newProduct:Product):void
-     * + lookupPart(partId:int):Part
-     * + lookupProduct(productId:int):Product
-     * + lookupPart(partName:String):ObservableList<Part>
-     * + lookupProduct(productName:String):ObservableList<Product>
-     * + updatePart(index:int, selectedPart:Part):void
-     * + updateProduct(index:int, newProduct:Product):void
-     * + deletePart(selectedPart:Part):boolean
-     * + deleteProduct(selectedProduct:Product):boolean
-     * + getAllParts():ObservableList<Part>
-     * + getAllProducts():ObservableList<Product>
-     */
-
-
-
-
-
-
 }
